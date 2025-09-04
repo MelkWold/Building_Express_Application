664a2adb638db3ed5139e836d84a5c7d6205ece1
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-09-03 21:23:41 -0400

    ejs-based templating added

--- server.mjs
+++ server.mjs
@@ -9,23 +9,42 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// user routes
+// app.use("/", routes)
 
 // Using static styles
 app.use(express.static("./styles"));
 
+// ============================== Using EJS ===================================
 
-// user routes
-app.use("/", routes)
-
-
-// Building Template Engine using ejs
+// St up View Engine using ejs
+    
     // step 1: set EJS at the view engine
     app.set("view engine", "ejs");
 
     // step 2: set the views folder as ./views
     app.set("views", "./views");
 
-    // step 3
+    // step 3: Render contents from the server to the home page
+    app.get("/", (req, res) => {
+        console.log("It's working")
+        res.render ("home", {
+            title: "Welcome to the Homepage",
+            text1: "This webpage is kindly dedicated to you; the user! Please interact with our web platform in a manner that brings you joy and fullfillment. May peace be unto you!",
+            text2: "And now, the links: "
+        })
+    });
+
+    app.get("/hobbies", (req, res) => {
+        res.render("hobbies");
+    }); 
+
+    app.get("/countriesVisited", (req, res) => {
+        res.render("countriesVisited");
+    })
+
+
+
 
 
 
